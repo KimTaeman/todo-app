c3aed04520cb5b54586cce1d90207b17cbdee6d9
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2025-04-30 22:02:00 +0700

    Updated UI/UX

--- todo-frontend/src/App.tsx
+++ todo-frontend/src/App.tsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { Button } from "./components/ui/button";
-import { IoIosAdd } from "react-icons/io";
-import TodoCard from "./components/my-components/TodoCard.jsx";
-import { Axios } from "../axiosInstance.ts";
+import React, { useState, useEffect } from 'react';
+import { Button } from './components/ui/button';
+import { IoIosAdd } from 'react-icons/io';
+import TodoCard from './components/my-components/TodoCard.jsx';
+import { Axios } from '../axiosInstance.ts';
 import {
   createTodo,
   getTodo,
   updateTodoName,
   deleteTodo,
   updateTodoStatus,
-} from "./api/todo.ts";
+} from './api/todo.ts';
 
-import "./App.css";
+import './App.css';
 
 const App = () => {
   const testConnection = async () => {
     try {
-      const data = await Axios.get("/");
+      const data = await Axios.get('/');
       console.log(data.data);
     } catch (e) {
       console.log(`Error fetching backend server: ${e}`);
@@ -46,54 +46,50 @@ const App = () => {
   };
 
   const [todo, setTodo] = useState<Todo[]>([]);
-
   const [editingId, setEditingId] = useState<number | null>(null);
-  const [editingText, setEditingText] = useState<string>("");
-  const [newTodoText, setNewTodoText] = useState<string>("");
+  const [editingText, setEditingText] = useState<string>('');
+  const [newTodoText, setNewTodoText] = useState<string>('');
 
-  // Handle adding new todo
+  // Separate todos into active and completed
+  const incompletedTodos = todo.filter((todo) => !todo.success);
+  const completedTodos = todo.filter((todo) => todo.success);
+
   const handleAddTodo = async () => {
-    if (newTodoText.trim() === "") return;
+    if (newTodoText.trim() === '') return;
 
     const result = await createTodo(newTodoText);
     if (result.success) {
       setTodo((prev) => [...prev, result.data.data]);
-      setNewTodoText("");
+      setNewTodoText('');
     }
   };
 
-  // Start editing a todo
   const handleEditClick = (todo: Todo) => {
     setEditingId(todo.id);
     setEditingText(todo.name);
   };
 
-  // Save edited todo
   const handleSaveClick = async (id: number) => {
-    // Make the API call to update the todo on the server
     const updatedTodo = await updateTodoName(id, editingText);
 
     if (updatedTodo) {
-      // Update the todo in the local state if the update is successful
       setTodo((prev) =>
         prev.map((todo) =>
           todo.id === id ? { ...todo, name: editingText } : todo
         )
       );
       setEditingId(null);
-      setEditingText("");
+      setEditingText('');
     } else {
-      // Handle error if needed, for example, show a message to the user
-      alert("Failed to save the edited todo.");
+      alert('Failed to save the edited todo.');
     }
   };
 
-  // Handle input key events (Enter to save or add)
   const handleInputKeyDown = (
     e: React.KeyboardEvent<HTMLInputElement>,
     id?: number
   ) => {
-    if (e.key === "Enter") {
+    if (e.key === 'Enter') {
       if (editingId !== null && id !== undefined) {
         handleSaveClick(id);
       } else {
@@ -102,7 +98,6 @@ const App = () => {
     }
   };
 
-  // Toggle success status
   const toggleSuccess = async (id: number) => {
     const toggle = await updateTodoStatus(id);
     if (toggle) {
@@ -112,58 +107,103 @@ const App = () => {
         )
       );
     } else {
-      alert("Failed to update status");
+      alert('Failed to update status');
     }
   };
 
-  // Delete a todo
   const handleDelete = async (id: number) => {
     const removeTodo = await deleteTodo(id);
     if (removeTodo) {
       setTodo((prev) => prev.filter((todo) => todo.id !== id));
     } else {
-      // Handle error if needed, for example, show a message to the user
-      alert("Failed to delete the edited todo.");
+      alert('Failed to delete the edited todo.');
     }
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="bg-blue-300 flex flex-col gap-5 items-center justify-center p-10 w-100 h-100 rounded-2xl">
-        {/* Add Todo Section */}
-        <div className="flex bg-white rounded-2xl px-10 py-2 gap-2">
-          <Button variant="outline" size="sm" onClick={handleAddTodo}>
-            <IoIosAdd />
-          </Button>
-          <input
-            type="text"
-            className="px-2 py-1 rounded-md"
-            placeholder="Add new todo..."
-            value={newTodoText}
-            onChange={(e) => setNewTodoText(e.target.value)}
-            onKeyDown={(e) => handleInputKeyDown(e)}
-          />
-        </div>
-
-        {/* Todo List */}
-
-        <div className="flex flex-col gap-5">
-          {todo.map((todo) => (
-            <TodoCard
-              key={todo.id}
-              id={todo.id}
-              success={todo.success}
-              name={todo.name}
-              handleSuccessToggle={() => toggleSuccess(todo.id)}
-              handleSave={() => handleSaveClick(todo.id)}
-              handleEdit={() => handleEditClick(todo)}
-              handleDelete={() => handleDelete(todo.id)}
-              editingId={editingId}
-              editingText={editingText}
-              handleInputChange={(e) => setEditingText(e.target.value)}
-              handleKeyDown={(e) => handleInputKeyDown(e, todo.id)}
+    <div className='flex items-center justify-center h-screen main-bg bg-cover w-screen p-3 sm:p-8 lg:p-15'>
+      {/* Todo Form Container */}
+      <div className='bg-[var(--todo-container-bg)] flex flex-col gap-10 md:gap-15 justify-center p-10 w-full rounded-2xl'>
+        <h1 className='text-2xl lg:text-4xl font-bold'>Todo List</h1>
+        <div className='text-xs md:text-lg lg:text-xl flex flex-col gap-5'>
+          {/* Add Todo Section */}
+          <div className='flex bg-violet-400 text-white rounded-2xl px-10 py-2 gap-2'>
+            <Button variant='none' size='sm' onClick={handleAddTodo}>
+              <IoIosAdd />
+            </Button>
+            <input
+              type='text'
+              className='px-2 py-1 rounded-md placeholder:text-gray-200 text-white outline-none selection:text-white selection:bg-purple-300 text-sm lg:text-xl w-full'
+              placeholder='Add new todo...'
+              value={newTodoText}
+              onChange={(e) => setNewTodoText(e.target.value)}
+              onKeyDown={(e) => handleInputKeyDown(e)}
             />
-          ))}
+          </div>
+
+          {/* Incompleted Todos Section */}
+          <div className='flex flex-col gap-5'>
+            {incompletedTodos.length === 0 && todo.length > 0 ? (
+              <div className='text-center text-gray-500 bg-[#ffffff64] w-40 h-40 mx-auto flex justify-center items-center rounded-2xl font-semibold text-gray-400'>
+                All tasks completed!
+              </div>
+            ) : (
+              <>
+                {incompletedTodos.length > 0 && (
+                  <>
+                    {incompletedTodos.map((todo) => (
+                      <TodoCard
+                        key={todo.id}
+                        id={todo.id}
+                        success={todo.success}
+                        name={todo.name}
+                        handleSuccessToggle={() => toggleSuccess(todo.id)}
+                        handleSave={() => handleSaveClick(todo.id)}
+                        handleEdit={() => handleEditClick(todo)}
+                        handleDelete={() => handleDelete(todo.id)}
+                        editingId={editingId}
+                        editingText={editingText}
+                        handleInputChange={(e) =>
+                          setEditingText(e.target.value)
+                        }
+                        handleKeyDown={(e) => handleInputKeyDown(e, todo.id)}
+                      />
+                    ))}
+                  </>
+                )}
+              </>
+            )}
+          </div>
+
+          {/* Completed Todos Section */}
+          {completedTodos.length > 0 && (
+            <div className='flex flex-col gap-5 mt-5'>
+              <h2 className='font-semibold text-gray-700'>Completed</h2>
+              {completedTodos.map((todo) => (
+                <TodoCard
+                  key={todo.id}
+                  id={todo.id}
+                  success={todo.success}
+                  name={todo.name}
+                  handleSuccessToggle={() => toggleSuccess(todo.id)}
+                  handleSave={() => handleSaveClick(todo.id)}
+                  handleEdit={() => handleEditClick(todo)}
+                  handleDelete={() => handleDelete(todo.id)}
+                  editingId={editingId}
+                  editingText={editingText}
+                  handleInputChange={(e) => setEditingText(e.target.value)}
+                  handleKeyDown={(e) => handleInputKeyDown(e, todo.id)}
+                />
+              ))}
+            </div>
+          )}
+
+          {/* No todos at all */}
+          {todo.length === 0 && (
+            <div className='text-center text-gray-500 bg-[#ffffff64] w-40 h-40 mx-auto flex justify-center items-center rounded-2xl font-semibold text-gray-400'>
+              No todos available :(
+            </div>
+          )}
         </div>
       </div>
     </div>
--- todo-frontend/src/components/my-components/TodoCard.jsx
+++ todo-frontend/src/components/my-components/TodoCard.jsx
@@ -18,7 +18,10 @@ const TodoCard = ({
   return (
     <div key={id} className='flex gap-5 items-center bg-white rounded-2xl p-5'>
       {/* Success Toggle */}
-      <span onClick={handleSuccessToggle} className='cursor-pointer text-2xl'>
+      <span
+        onClick={handleSuccessToggle}
+        className='cursor-pointer text-2xl text-violet-400'
+      >
         {success ? <FaRegDotCircle /> : <FaRegCircle />}
       </span>
 
@@ -28,26 +31,40 @@ const TodoCard = ({
           value={editingText}
           onChange={handleInputChange}
           onKeyDown={handleKeyDown}
-          className='border-b border-gray-400 outline-none'
+          className='border-b border-gray-400 outline-none break-words w-full'
         />
       ) : (
-        <h1 className={success ? 'line-through' : ''}>{name}</h1>
+        <h1
+          className={`${
+            success ? 'line-through text-violet-800' : ''
+          } break-words overflow-wrap-break-word whitespace-normal flex-grow min-w-0`}
+        >
+          {name}
+        </h1>
       )}
 
       {/* Buttons */}
-      {editingId === id ? (
-        <Button variant='outline' size='sm' onClick={handleSave}>
-          Save
-        </Button>
-      ) : (
-        <Button variant='outline' size='sm' onClick={handleEdit}>
-          Edit
-        </Button>
-      )}
+      <div className='ml-auto flex  gap-2'>
+        {editingId === id ? (
+          <Button variant='outline' size='sm' onClick={handleSave}>
+            Save
+          </Button>
+        ) : (
+          <Button variant='outline' size='sm' onClick={handleEdit}>
+            Edit
+          </Button>
+        )}
 
-      <Button variant='outline' size='sm' onClick={handleDelete}>
-        X
-      </Button>
+        <Button
+          variant='outline'
+          size='sm'
+          onClick={handleDelete}
+          className={success ? 'cursor-not-allowed ' : ''}
+          disabled={success}
+        >
+          X
+        </Button>
+      </div>
     </div>
   );
 };
